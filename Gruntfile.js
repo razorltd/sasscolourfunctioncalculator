809e48a50876aa3a0dbdfc432f8b25c5b29f4335
Laurie Nicholas <laurie.nicholas@razorjam.co.uk>
2015-05-06 14:22:56 +0100

    Dependencies and grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Live Reload
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt) {
+
+  // Load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.loadNpmTasks('assemble');
+
+  var path = require('path');
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      options: {
+        livereload: true,
+        interrupt: true,
+      },
+      sass: {
+        files: ['styles/**/*.scss'],
+        tasks: ['sass']
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9090,
+        hostname: '0.0.0.0' // change this to '0.0.0.0' to access the server from outside
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              require('connect-livereload')(), // <--- here
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'dist')
+            ];
+          }
+        }
+      },
+    },
+
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>/'
+      }
+    },
+    
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'styles/main.css' : 'styles/main.scss'
+        }
+      }
+    },
+
+  });
+
+  grunt.registerTask('serve', function (target) {
+    grunt.task.run([
+      'sass',
+      'connect:livereload',
+      'open',
+      'watch',
+    ]);
+  });
+
+  grunt.registerTask('default', [
+    'serve'
+  ]);
+
+};
