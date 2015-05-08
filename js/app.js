8788f5ac4d277233a5689683ed2915e01329fc2f
Laurie Nicholas <laurie.nicholas@razorjam.co.uk>
2015-05-08 15:58:53 +0100

    Working in Angular

--- js/app.js
+++ js/app.js
@@ -1,47 +1,42 @@
-(function($) {
-	// $(function() {
-		var colorA = '#BadA55',
-				colorB = '#B0BAAA';
+angular.module('scfc', [])
+  .controller('ScfcController', ['$scope', function($scope) {
+    $scope.scfc = {
+    	colorA: '#BADA55',
+    	colorB: '#B0BCA7',
+    	colorDiff: function(a,b) {
+    		var a = tinycolor(a).toHsl(),
+    				b = tinycolor(b).toHsl(),
 
-		var colorDiff = function(a, b) {
-			var a = tinycolor(a).toHsl(),
-					b = tinycolor(b).toHsl(),
+    				sat = a.s - b.s,
+    				lig = a.l - b.l,
+    				hue = -(a.h - b.h),
 
-					sat = a.s - b.s,
-					lig = a.l - b.l,
-					hue = -(a.h - b.h),
+    				fnSat = (sat > 0) ? 'desaturate' : 'saturate',
+    				fnLig = (lig > 0) ? 'darken' : 'lighten';
 
-					fnSat = (sat > 0) ? 'desaturate' : 'saturate',
-					fnLig = (lig > 0) ? 'darken' : 'lighten';
+    		sat = Math.abs(sat) * 100;
+    		lig = Math.abs(lig) * 100;
 
-			sat = Math.abs(sat) * 100;
-			lig = Math.abs(lig) * 100;
+    		return {
+    			baseColor: '#' + tinycolor(a).toHex(),
+    			fnHue : 'adjust-hue',
+    			hue : hue,
+    			fnSat : fnSat,
+    			sat : sat,
+    			fnLig : fnLig,
+    			lig: lig
+    		}
+    	},
+    	constructAdjustmentString: function(diff) {
+    		var t1 = diff.fnHue + '(' + diff.baseColor + ', ' + diff.hue + ')',
+    				t2 = diff.fnSat  + '(' + t1 + ', ' + diff.sat + ')',
+    				t3 = diff.fnLig  + '(' + t2 + ', ' + diff.lig + ')';
 
-			return {
-				baseColor: '#' + tinycolor(a).toHex(),
-				fnHue : 'adjust-hue',
-				hue : hue,
-				fnSat : fnSat,
-				sat : sat,
-				fnLig : fnLig,
-				lig: lig
-			}
-		};
-
-		var constructAdjustmentString = function(diff) {
-			var t1 = diff.fnHue + '(' + diff.baseColor + ', ' + diff.hue + ')',
-					t2 = diff.fnSat  + '(' + t1 + ', ' + diff.sat + ')',
-					t3 = diff.fnLig  + '(' + t2 + ', ' + diff.lig + ')';
-
-			return t3;
-		}
-
-
-		var adjustments = colorDiff(colorA, colorB);
-		var adjustmentString = constructAdjustmentString(adjustments);
-
-		console.log(adjustmentString);
-
-
-	// });
-})(jQuery);
+    		return t3;
+    	},
+    	adjustmentString: function() {
+    		var adjustments = $scope.scfc.colorDiff($scope.scfc.colorA, $scope.scfc.colorB);
+    		return $scope.scfc.constructAdjustmentString(adjustments);
+    	}
+    };
+  }]);
