c74cf2e82f004bcf99bd44f74e14d41c7e4e4fa4
Laurie Nicholas <laurienicholas@RazorLaurie.local>
2017-03-27 13:59:37 +0100

    Improving accuracy

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ angular.module('scfc', [])
             colorB: '#B0BCA7',
             colorDiff: function(a,b) {
                 var a = tinycolor(a).toHsl(),
-                        b = tinycolor(b).toHsl(),
+                    b = tinycolor(b).toHsl(),
   
-                        sat = a.s - b.s,
-                        lig = a.l - b.l,
-                        hue = -(a.h - b.h),
+                    sat = a.s - b.s,
+                    lig = a.l - b.l,
+                    hue = -(a.h - b.h),
   
-                        fnSat = (sat > 0) ? 'desaturate' : 'saturate',
-                        fnLig = (lig > 0) ? 'darken' : 'lighten';
+                    fnSat = (sat > 0) ? 'desaturate' : 'saturate',
+                    fnLig = (lig > 0) ? 'darken' : 'lighten';
   
                 sat = Math.abs(sat) * 100;
                 lig = Math.abs(lig) * 100;
@@ -22,35 +22,34 @@ angular.module('scfc', [])
                     fnHue : 'adjust-hue',
                     hue : hue.toFixed(0),
                     fnSat : fnSat,
-                    sat : sat.toFixed(0),
+                    sat : sat.toFixed(2),
                     fnLig : fnLig,
-                    lig: lig.toFixed(0)
+                    lig: lig.toFixed(2)
                 }
             },
             adjustmentStringConstuctor: function(diff) {
                 var addTransformToString = function(diffVal, diffFn, transformString) {
-                    if (diffVal != 0) {
+                    if (diffVal != 0)
                         return diffFn + '(' + transformString + ', ' + diffVal + ')';
-                    } else {
-                        return transformString;
-                    }
+                    
+                    return transformString;
                 }
 
-                var t = diff.baseColor;
+                var transformString = diff.baseColor;
 
-                t = addTransformToString(diff.hue, diff.fnHue, t);
-                t = addTransformToString(diff.sat, diff.fnSat, t);
-                t = addTransformToString(diff.lig, diff.fnLig, t);
+                transformString = addTransformToString(diff.hue, diff.fnHue, transformString);
+                transformString = addTransformToString(diff.sat, diff.fnSat, transformString);
+                transformString = addTransformToString(diff.lig, diff.fnLig, transformString);
 
-                if (t === diff.baseColor) {
+                if (transformString === diff.baseColor)
                     return "Colours are too similar, pal!";
-                }
 
-                return t;
+                return transformString;
             },
             adjustmentString: function() {
                 if ( !( tinycolor($scope.scfc.colorA).isValid() && tinycolor($scope.scfc.colorB).isValid() ) )
-                        return 'Please enter two valid colours';
+                    return 'Please enter two valid colours';
+                
                 var adjustments = $scope.scfc.colorDiff($scope.scfc.colorA, $scope.scfc.colorB);
                 return $scope.scfc.adjustmentStringConstuctor(adjustments);
             }
